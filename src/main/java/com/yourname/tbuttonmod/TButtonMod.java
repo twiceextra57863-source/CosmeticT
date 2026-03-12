63c9421566ea2e8162a9b224f9cb9705da34ebef
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-13 00:27:09 +0530

    Update TButtonMod.java

--- src/main/java/com/yourname/tbuttonmod/TButtonMod.java
+++ src/main/java/com/yourname/tbuttonmod/TButtonMod.java
@@ -1,16 +1,21 @@
 package com.yourname.tbuttonmod;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TButtonMod implements ModInitializer {
     public static final String MOD_ID = "tbuttonmod";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
+    
     @Override
     public void onInitialize() {
-        LOGGER.info("TButtonMod Initialized!");
-        FolderManager.init();
+        LOGGER.info("TButton Mod initialized!");
+        FolderManager.createFolders();
     }
 }
