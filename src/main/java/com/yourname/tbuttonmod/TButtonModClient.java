80ad1d518dc3e3a64d5ca4fc88080bffb16b32f9
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-13 00:26:51 +0530

    Update TButtonModClient.java

--- src/main/java/com/yourname/tbuttonmod/TButtonModClient.java
+++ src/main/java/com/yourname/tbuttonmod/TButtonModClient.java
@@ -1,21 +1,33 @@
 package com.yourname.tbuttonmod;
 
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-public class TButtonMod implements ModInitializer {
-    public static final String MOD_ID = "tbuttonmod";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+public class TButtonModClient implements ClientModInitializer {
+    public static KeyBinding openDashboardKey;
     
     @Override
-    public void onInitialize() {
-        LOGGER.info("TButton Mod initialized!");
-        FolderManager.createFolders();
+    public void onInitializeClient() {
+        openDashboardKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            "key.tbuttonmod.dashboard",
+            InputUtil.Type.KEYSYM,
+            GLFW.GLFW_KEY_T,
+            "category.tbuttonmod"
+        ));
+        
+        ScreenEvents.BEFORE_INIT.register((client, screen, scaledWidth, scaledHeight) -> {
+            if (screen instanceof TitleScreen) {
+                ScreenEvents.afterRender(screen).register((screen1, matrices, mouseX, mouseY, tickDelta) -> {
+                    // Add T button to title screen
+                });
+            }
+        });
     }
 }
