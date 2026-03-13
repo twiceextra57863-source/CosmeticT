b81d6ea79e876709490a09960cab35c31085e4d9
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-13 07:25:59 +0530

    Update TButtonModClient.java

--- src/main/java/com/yourname/tbuttonmod/TButtonModClient.java
+++ src/main/java/com/yourname/tbuttonmod/TButtonModClient.java
@@ -1,38 +1,32 @@
 package com.yourname.tbuttonmod;
 
-import com.yourname.tbuttonmod.gui.DashboardScreen; // Add this import
+import com.yourname.tbuttonmod.gui.DashboardScreen;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
+import net.fabricmc.fabric.api.client.screen.v1.Screens;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.text.Text;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class TButtonModClient implements ClientModInitializer {
-    private static final Logger LOGGER = LoggerFactory.getLogger("TButtonClient");
     
     @Override
     public void onInitializeClient() {
+        // Register AFTER_INIT event
         ScreenEvents.AFTER_INIT.register((client, screen, scaledWidth, scaledHeight) -> {
             if (screen instanceof TitleScreen) {
-                addTButton((TitleScreen) screen);
+                // FIX: Use Screens.getButtons() to add button
+                ButtonWidget tButton = ButtonWidget.builder(
+                    Text.literal("T"),
+                    button -> client.setScreen(new DashboardScreen((TitleScreen) screen))
+                )
+                .dimensions(screen.width - 40, 10, 30, 30)
+                .build();
+                
+                // This is the correct way - Screens.getButtons() returns a list we can add to
+                Screens.getButtons(screen).add(tButton);
             }
         });
-    }
-    
-    private void addTButton(TitleScreen screen) {
-        int width = MinecraftClient.getInstance().getWindow().getScaledWidth();
-        
-        ButtonWidget tButton = ButtonWidget.builder(
-            Text.literal("T"),
-            button -> MinecraftClient.getInstance().setScreen(new DashboardScreen(screen))
-        )
-        .dimensions(width - 40, 10, 30, 30)
-        .build();
-        
-        screen.addDrawableChild(tButton);
-        LOGGER.info("T button added to TitleScreen");
     }
 }
