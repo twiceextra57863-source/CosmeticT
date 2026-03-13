181b4998d1cb57da6e686de06c7a835d83ba133a
twiceextra57863-source <twiceextra57863@gmail.com>
2026-03-13 07:10:37 +0530

    Update FolderManager.java

--- src/main/java/com/yourname/tbuttonmod/FolderManager.java
+++ src/main/java/com/yourname/tbuttonmod/FolderManager.java
@@ -3,36 +3,39 @@ package com.yourname.tbuttonmod;
 import net.minecraft.client.MinecraftClient;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 
 public class FolderManager {
     
     public static void createFolders() {
-        MinecraftClient client = MinecraftClient.getInstance();
-        File gameDir = client.runDirectory;
-        
-        // Create main TCosmetics folder
-        File tCosmeticFolder = new File(gameDir, "TCosmetics");
-        if (!tCosmeticFolder.exists()) {
-            tCosmeticFolder.mkdirs();
-        }
-        
-        // Create skins subfolder
-        File skinsFolder = new File(tCosmeticFolder, "skins");
-        if (!skinsFolder.exists()) {
-            skinsFolder.mkdirs();
-        }
-        
-        // Create README file
-        File readme = new File(tCosmeticFolder, "README.txt");
-        if (!readme.exists()) {
-            try {
-                readme.createNewFile();
-                // Write instructions
-                java.nio.file.Files.write(readme.toPath(), 
-                    "Place your skin PNG files in the 'skins' folder to use them in the T-Cosmetics dashboard!".getBytes());
-            } catch (IOException e) {
-                e.printStackTrace();
+        try {
+            MinecraftClient client = MinecraftClient.getInstance();
+            File gameDir = client.runDirectory;
+            
+            File tCosmeticFolder = new File(gameDir, "TCosmetics");
+            if (!tCosmeticFolder.exists()) {
+                tCosmeticFolder.mkdirs();
             }
+            
+            File skinsFolder = new File(tCosmeticFolder, "skins");
+            if (!skinsFolder.exists()) {
+                skinsFolder.mkdirs();
+            }
+            
+            // Create README
+            File readme = new File(tCosmeticFolder, "README.txt");
+            if (!readme.exists()) {
+                String content = "TButton Mod - Skin Installation Guide\n" +
+                               "====================================\n\n" +
+                               "1. Place your skin PNG files in the 'skins' folder\n" +
+                               "2. Launch Minecraft and click the T button\n\n" +
+                               "Folder location: " + skinsFolder.getAbsolutePath();
+                Files.write(readme.toPath(), content.getBytes());
+            }
+            
+            TButtonMod.LOGGER.info("TCosmetics folders created at: " + tCosmeticFolder.getAbsolutePath());
+        } catch (Exception e) {
+            TButtonMod.LOGGER.error("Failed to create folders", e);
         }
     }
 }
